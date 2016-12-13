b50cc42335f071343ec2ebea9058cff3b5ceaea2
Richard Luo <richardluo0506@gmail.com>
2016-12-13 04:41:03 -0800

    hue shift

--- app/core.controller.js
+++ app/core.controller.js
@@ -19,11 +19,23 @@
             core.menuIsOpen = false;
         }
 
-        function toggleMenu() {
-            var top = $('body').scrollTop();
-            $('body').scrollTop(0);
-            $('.ui-vv').css({marginTop: -top});
-            core.menuIsOpen = !core.menuIsOpen;
+        function toggleMenu(open) {
+            console.log('', open);
+            if (open) {
+                var top = $('body').scrollTop();
+                $('body').scrollTop(0);
+                $('.ui-vv').css({ marginTop: -top });
+                core.menuIsOpen = true;
+            } else {
+                var top = $('.ui-vv').css('marginTop');
+                core.menuIsOpen = false;
+                var scrollTop = top.slice(0, -2) * -1;
+                console.log('', scrollTop);
+                $('.ui-vv').css({ marginTop: 0 });
+                setTimeout(function() {
+                    $('body').scrollTop(top.slice(0, -2) * -1);
+                }, 0);
+            }
         }
     }
 })();
