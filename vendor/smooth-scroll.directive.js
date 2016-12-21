0f3f7cb70934fb01513e7a84d01ba8c29e6963a3
Richard Luo <richardluo0506@gmail.com>
2016-12-20 17:45:56 -0800

    adding scroll reveal

---
+++ vendor/smooth-scroll.directive.js
@@ -0,0 +1,44 @@
+  (function() {
+      'use strict';
+      // Include app dependency on ngMaterial 
+
+      angular
+          .module('App')
+          .directive('uiSmoothScroll', uiSmoothScroll);
+
+      uiSmoothScroll.$inject = [];
+
+      function uiSmoothScroll() {
+          return {
+              restrict: 'A',
+              link: function($scope, $element, $attrs) {
+
+                  function onClick(ev) {
+                      ev.preventDefault();
+                      var target = document.querySelector($attrs.href);
+                      if (!target) return;
+                      var dy = target.getBoundingClientRect().top;
+                      var duration = parseInt($scope.$eval($attrs.uiSmoothScroll)) || 300;
+                      var startTime = Date.now(),
+                          startY = window.scrollY;
+
+                      animate();
+
+                      function animate() {
+                          var progress = Math.min((Date.now() - startTime) / duration, 1);
+                          window.scrollTo(0, startY + dy * progress);
+                          if (progress < 1)
+                              window.requestAnimationFrame(animate);
+                      }
+                  }
+
+                  $element.on('click', onClick);
+
+                  $element.on('$destroy', function() {
+                      $element.off('click', onClick);
+                  });
+
+              }
+          };
+      }
+  })();
