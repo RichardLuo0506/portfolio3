352c4368e1d4e7ae9e36b451beeca27afa7d36af
Richard Luo <richardluo0506@gmail.com>
2017-01-08 22:30:00 -0800

    working mailer

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -6,14 +6,30 @@
         .module('App')
         .controller('contact', contact);
 
-    contact.$inject = [];
+    contact.$inject = ['$timeout'];
 
-    function contact() {
+    function contact($timeout) {
         var vm = this;
 
         init();
 
+        vm.sendMail = sendMail;
+
         function init() {
+            vm.btnText = "SEND MESSAGE";
+        }
+
+        function sendMail() {
+            vm.btnText = "WORKING...";
+            emailjs.send("richard_luo", "richard_luo_io", { from_name: vm.fromName, from_email: vm.fromEmail, body: vm.body }).then(function(res) {
+                if (res.text === 'OK') {
+                    $timeout(function() {
+                        vm.btnText = 'SENT!';
+                    }, 500);
+                }
+            }, function(err) {
+                console.log('', err);
+            });
         }
     }
 })();
